8bcc0319e04297730c0b5dd42f977575f7b5734a
Sebastian Abril <juanseabril@gmail.com>
2023-06-28 05:21:59 -0500

    Proyecto terminado

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -25,7 +25,7 @@ const UserProfile = ({ params }) => {
   return (
     <Profile
       name={userName}
-      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      desc={`Bienvenido a la página de perfil personalizado de ${userName}`}
       data={userItems}
     />
   );
